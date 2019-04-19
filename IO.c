a04595b242f2d2b9cf77a9fc5a54655de152b3de
huangmj7 <huangm7@rpi.edu>
2019-04-18 22:11:31 -0400

    IO update

--- IO.c
+++ IO.c
@@ -10,6 +10,7 @@ int k = -1;
 
 double *a = NULL;
 double *b = NULL;
+double *c = NULL;
 
 /*input functions*/
 
@@ -27,45 +28,62 @@ int input(char* files,double* A, double* B){
                   perror("FAIL TO READ INPUT FILE");
                   return -1;
         }
-        
-	char* x = calloc(m*k,sizeof(char));
-	char* y = calloc(k*n,sizeof(char));
-        fscanf( my_input_file, "%s %s", x, y );
-	for(int i=0; i<(m*k); i++){A[i] =  (double)(x[i] - '0');}
-	for(int j=0; j<(k*n); j++){b[j] =  (double)(y[j] - '0');}
+       
+       	
+	for(int i=0; i<(m*k); i++){if(!fscanf(my_input_file,"%lf",&A[i])){break;}}
+	for(int i=0; i<(m*k); i++){if(!fscanf(my_input_file,"%lf",&B[i])){break;}}
+
 	
         fclose(my_input_file);
-	free(x);
-	free(y);
         return 0;
         
 
 }
 
+int output(char* file, double* C){
+
+	if(sizeof(file) == 0 || m == -1 || n == -1){
+		perror("ERROR: No input is given for writing");
+		return -1;
+	}
+
+	FILE *my_output_file = fopen(file,"w");
+	for(int i=0; i<m; i++){
+		for(int j=0; j<n; j++){
+
+			fprintf(my_output_file,"%lf ",C[i*n+j]);
+		}
+		fprintf(my_output_file,"\n");
+	}
+	return 0;
+}
+
 int main(int argc,char**argv){
 
-	if(argc < 4){
+	if(argc < 5){
 		perror("ERROR: NOT ENOUGH INPUT");
 		return -1;
 	}
 /*
  * argv[1] input file
- * argv[2] m 
- * argv[3] n 
- * argv[4] k 
+ * argv[2] output file
+ * argv[3] m 
+ * argv[4] n 
+ * argv[5] k 
  * A m*k
  * B k*n
  * C m*n 
 */
-	m = atoi(argv[2]);
-	n = atoi(argv[3]);
-	k = atoi(argv[4]);
+	m = atoi(argv[3]);
+	n = atoi(argv[4]);
+	k = atoi(argv[5]);
         
 	a = calloc(m*k,sizeof(double));
 	b = calloc(k*n,sizeof(double));
+	c = calloc(m*n,sizeof(double));
 
 	input(argv[1],a,b);
-
+        
 	for(int i=0; i<k; i++){
 		for(int j=0; j<n; j++){
 			printf("%le ",b[i*n+j]);
@@ -73,6 +91,12 @@ int main(int argc,char**argv){
 		printf("\n");
 	}
 
+	for(int l=0; l<(m*n); l++){c[l] = l;}
+	output(argv[2],c);
+       
+        free(a);
+	free(b);
+	free(c);	
 	return 0;
 
 }
